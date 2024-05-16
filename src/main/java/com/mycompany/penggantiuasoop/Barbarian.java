58426597e8b77448524a2da14b1a925e85cf1aea
FDN0828 <ferdiandn08@gmail.com>
2024-05-16 19:42:29 +0700

    Buat Class Barbarian

---
+++ src/main/java/com/mycompany/penggantiuasoop/Barbarian.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.penggantiuasoop;
+
+/**
+ *
+ * @author USER
+ */
+class Barbarian extends Enemy{
+    public Barbarian(String name, int hp, int physicalAttack, int magicAttack, int pDefense, int mgDefense) {
+            super(name, hp, physicalAttack, magicAttack, pDefense, mgDefense);
+    }
+    
+    @Override
+    public void attack(){
+         String skillChoice = chooseAttackSkill();
+        if (skillChoice.equals("attack")) {
+            basicAttack();
+        } else if (skillChoice.equals("strike")) {
+            strikeAttack();
+        } else if (skillChoice.equals("magicAttack")) {
+            magicAttack();
+        }else if(skillChoice.equals("Use Skill")) {
+            useSkill();
+        }else {
+            System.out.println("Skill tidak valid!");
+        }
+        
+    }
+    
+    @Override
+    public void defend(){
+        String skillChoice = chooseDefenseSkill();
+        if (skillChoice.equals("defend")) {
+            basicDefend();
+        } else if (skillChoice.equals("counter")) {
+            counter();
+        } else if (skillChoice.equals("giveUp")) {
+            giveUp();
+        } else if (skillChoice.equals("magicDefend")) {
+            magicDefend();
+        } else {
+            System.out.println("Skill tidak valid!");
+        }
+    }
+
+    
+
+  
+    
+    
+
+    
+    
+}
