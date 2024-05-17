2fa505da8485913f5390ed736472083aab7c9e43
Adwira <aryadwiputra1284@gmail.com>
2024-05-17 23:02:37 +0700

    isi action

--- src/main/java/com/mycompany/penggantiuasoop/Action.java
+++ src/main/java/com/mycompany/penggantiuasoop/Action.java
@@ -1,5 +1,43 @@
-package com.mycompany;
+package com.mycompany.penggantiuasoop;
 
 public class Action {
-
+    
+    public void att_Defend(){
+        
+    }
+    
+    public void att_MDef(){
+        
+    }
+    
+    public void att_Counter(){
+        
+    }
+    
+    public void strike_Defend(){
+        
+    }
+    
+    public void strike_Counter(){
+        
+    }
+    
+    public void strike_mDef(){
+        
+    }
+    
+    public void matk_Defend(){
+        
+    }
+    
+    public void matk_MDef(){
+        
+    }
+    
+    public void matk_Counter(){
+        
+    }
+    
+    
 }
+
